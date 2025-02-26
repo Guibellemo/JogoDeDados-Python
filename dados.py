f8beedca9015078afd1e2000d81ecca7f9ed80bf
Guibellemo <gbellemo@gmail.com>
2025-02-26 17:04:12 -0300

    adicionado função def main()

--- dados.py
+++ dados.py
@@ -75,20 +75,23 @@ class Rodada:
             if jogarNovamente == "N":
                 print("==========================================")
                 print("==========================================")
-                print(f"Total de pontos final: J1: {pontoRodadaJogador1} e J2: {pontoRodadaJogador2}")
+                print(f"♦ Total de pontos final: J1: {pontoRodadaJogador1} e J2: {pontoRodadaJogador2} ♦")
                 print("==========================================")
                 print("==========================================")
                 print("Até mais!")
                 break
 
+def main():
 
-nomeJogador1 = input("Seu nome: ")
-nomeJogador2 = input("Seu nome: ")
-j1 = JogadorUm(nomeJogador1)
-j2 = JogadorDois(nomeJogador2)
-j1.jogar()
-j2.jogar()
+    nomeJogador1 = input("Seu nome: ")
+    nomeJogador2 = input("Seu nome: ")
+    j1 = JogadorUm(nomeJogador1)
+    j2 = JogadorDois(nomeJogador2)
+    j1.jogar()
+    j2.jogar()
 
-rodada = Rodada()
-rodada.rodar(j1, j2)
+    rodada = Rodada()
+    rodada.rodar(j1, j2)
+
+main()
 
